b6f523be2c895dbac2aa81d4c145bb34884c3a53
Railos Course <mirkhan@gmail.com>
2016-01-25 21:16:57 +0000

    Add show and flash messagees

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,8 +8,16 @@ end
   def create
    
     @article = Article.new(article_param)
-    @article.save
-    redirect_to articles_show(@articles)
+    if @article.save
+      flash[:notice] = "Article is successfully created"
+      redirect_to article_path(@article)
+      
+    else
+      render 'new'
+    end
+  end
+  def show
+    @article = Article.find(params[:id])
   end
   
   private
