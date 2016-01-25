d7b83da2d45338770898300a7f11b1fb34dd6e8a
Railos Course <mirkhan@gmail.com>
2016-01-25 21:46:58 +0000

    Add edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,25 +3,38 @@ class ArticlesController <ApplicationController
 def new
   @article = Article.new
 end
-  
-  
-  def create
-   
-    @article = Article.new(article_param)
-    if @article.save
-      flash[:notice] = "Article is successfully created"
-      redirect_to article_path(@article)
-      
-    else
-      render 'new'
-    end
-  end
-  def show
+  def edit
     @article = Article.find(params[:id])
   end
   
+  def create
+   
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article is successfully created"
+      redirect_to article_path(@article)
+      else
+      render 'new'
+    end
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article is successfully Updated"
+      redirect_to article_path(@article)
+      else
+      render 'edit'
+    end
+      
+  end
+  def show
+    @article = Article.find(params[:id])
+    
+  end
+  
   private
-    def article_param
+    def article_params
       params.require(:article).permit(:title, :description)
     end
 end
