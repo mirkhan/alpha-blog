eb4b6d46fb9334e7506b0c725a097ccd8fdd32cf
Railos Course <mirkhan@gmail.com>
2016-02-08 17:57:57 +0000

    Add association with usr and article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,8 +13,9 @@ end
   end
   
   def create
-   
+   debugger
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
       flash[:success] = "Article is successfully created"
       redirect_to article_path(@article)
