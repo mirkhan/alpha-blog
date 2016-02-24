b1398d3df703f591accc31832ed235ce6a06a5a1
Railos Course <mirkhan@gmail.com>
2016-02-24 20:29:06 +0000

    Restriction on UI and controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,13 @@
 class ArticlesController <ApplicationController
   
   before_action :set_article, only: [:edit, :update , :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  
+  
   
   def index 
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
 def new
   @article = Article.new
@@ -13,9 +17,9 @@ end
   end
   
   def create
-   debugger
+  
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = User.last
     if @article.save
       flash[:success] = "Article is successfully created"
       redirect_to article_path(@article)
@@ -36,7 +40,6 @@ end
   end
   def show
   
-    
   end
   
   def destroy
@@ -50,7 +53,15 @@ end
   def set_article
      @article = Article.find(params[:id])
   end
+  
     def article_params
       params.require(:article).permit(:title, :description)
     end
+    
+    def require_same_user
+      if current_user  != @article.user
+        flash[:danger] = "You can only edit or delete your own article"
+        redirect_to root_path
+    end
+  end
 end
